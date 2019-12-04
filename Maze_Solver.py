98c4555316ed0a8d646e8b427a96a0f0506f33bd
Haley <haleybug234@gmail.com>
2019-12-04 15:14:01 -0700

    Update Maze_Solver.py

--- Maze_Solver.py
+++ Maze_Solver.py
@@ -1,3 +1,11 @@
 # Maze Solver algorithm
 
 # Michael Gini, Justin Davis, Scott Weygandt, Haley Drexel, Oscar Delgado
+
+import networkx as nx
+
+def SolveMaze(mazeGraph, start, end):
+  
+  path = nx.shortest_path(mazeGraph, start, end, weight='weight')
+  
+  return path
