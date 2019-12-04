3ff7a9fa22e21f4bb444b78a43bffdd37a5128c6
Scott Weygandt <scwe4009@colorado.edu>
2019-12-04 15:46:31 -0700

    Started coordinate converter node
    added node method and name line

--- Coordinate_Converter.py
+++ Coordinate_Converter.py
@@ -1,3 +1,14 @@
 # Coordinate Converter
 
 # Michael Gini, Justin Davis, Scott Weygandt, Haley Drexel, Oscar Delgado
+import rospy
+from std_msgs.msg import String
+
+def coordinateConverter():
+	rospy.init_node('CoordinateConverter', anonymous=True)
+
+if name == '___main___':
+	try:
+		coordinateConverter()
+	except rospy.ROSInterruptException:
+		pass
