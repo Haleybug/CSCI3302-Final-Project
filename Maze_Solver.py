80ea35cd58028319a633130877db87fb2179a482
Haley <haleybug234@gmail.com>
2019-12-04 15:35:23 -0700

    Added comments for wrapper function of maze solver

--- Maze_Solver.py
+++ Maze_Solver.py
@@ -4,6 +4,10 @@
 
 import networkx as nx
 
+# G is a Unweighted, Bidirectional NetworkX graph of the maze
+# start is the beginning node (maze start)
+# end is the end node (maze exit)
+
 def solveMaze(G, start, end):
   
   path = nx.bidirectional_shortest_path(G, start, end)
